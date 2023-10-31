c29f3b80d483ee6d17228d82f1759d449c39cf35
khoabaps <99518851+khoabaps@users.noreply.github.com>
2023-10-31 21:57:45 +0700

    commit DATN

--- src/main/java/com/example/demo/Controller/Admin/SanPhamController.java
+++ src/main/java/com/example/demo/Controller/Admin/SanPhamController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.Entity.SanPhamEntity;
 import com.example.demo.Service.SanPhamService; 
@@ -88,4 +89,5 @@ public class SanPhamController {
 //
 //		return "redirect:/Admin/SanPham";
 //	}
+
 }
