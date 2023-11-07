13037472c5062a964463c92edca175dfe0abf104
iamahacker1 <nguyenhoainam29.08.01@gmail.com>
2023-11-08 00:49:24 +0700

    commit cart

--- src/main/java/com/example/demo/Controller/User/DetailController.java
+++ src/main/java/com/example/demo/Controller/User/DetailController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.Dao.SanPhamDao;
 import com.example.demo.Entity.SanPhamEntity;
 import com.example.demo.Service.SanPhamService; 
 
@@ -17,15 +18,18 @@ public class DetailController {
 
 	@Autowired
 	SanPhamService SPService;
+	
+	@Autowired
+	SanPhamDao sanPhamDao;
 
 	@RequestMapping("/detail")
 	public String detail() {
 		return "user/product/detail";
 	}
 
-	@GetMapping("/detail/{id}")
-	public String details(Model model, @PathVariable("id") String id) {
-		SanPhamEntity item = SPService.findById(id);
+	@GetMapping("/detail/{id_sp}")
+	public String details(Model model, @PathVariable("id_sp") String id_sp) {
+		SanPhamEntity item = sanPhamDao.findById(id_sp).get();
 		model.addAttribute("item", item);
 		return "user/product/detail";
 	}
---
+++ src/main/java/com/example/demo/RestAPI/CartRestController.java
@@ -0,0 +1,26 @@
+package com.example.demo.RestAPI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Entity.SanPhamEntity;
+import com.example.demo.Service.SanPhamService;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/rest/products")
+public class CartRestController {
+	
+	@Autowired
+	SanPhamService sanPhamService;
+	
+	@GetMapping("{id_sp}")
+	public SanPhamEntity getById(@PathVariable("id_sp") String id_sp) {
+		return sanPhamService.findById(id_sp);
+	}
+
+}
