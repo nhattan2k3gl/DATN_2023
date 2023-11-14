99e87d62bf046d9ef9aae72fde2d711c1e6f2f42
khoabaps <99518851+khoabaps@users.noreply.github.com>
2023-11-14 21:26:23 +0700

    commit DATN

--- src/main/java/com/example/demo/Controller/Admin/YourController.java
+++ src/main/java/com/example/demo/Controller/Admin/YourController.java
@@ -1,26 +1,26 @@
-package com.example.demo.Controller.Admin;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.example.demo.Entity.SanPhamEntity;
-
-@Controller
-public class YourController {
-
-    @Autowired
-    private YourEntityRepository yourEntityRepository;
-
-    @GetMapping("/yourPage")
-    public String yourPage(Model model) {
-        // Truy vấn dữ liệu từ cơ sở dữ liệu, ví dụ:
-        SanPhamEntity SPEntity = yourEntityRepository.findById(yourEntityId).orElse(null);
-
-        // Đặt dữ liệu vào model để sử dụng trong Thymeleaf template
-        model.addAttribute("yourEntity", yourEntity);
-
-        return "yourPage";
-    }
-}
+//package com.example.demo.Controller.Admin;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//import com.example.demo.Entity.SanPhamEntity;
+//
+//@Controller
+//public class YourController {
+//
+//    @Autowired
+//    private YourEntityRepository yourEntityRepository;
+//
+//    @GetMapping("/yourPage")
+//    public String yourPage(Model model) {
+//        // Truy vấn dữ liệu từ cơ sở dữ liệu, ví dụ:
+//        SanPhamEntity SPEntity = yourEntityRepository.findById(yourEntityId).orElse(null);
+//
+//        // Đặt dữ liệu vào model để sử dụng trong Thymeleaf template
+//        model.addAttribute("yourEntity", yourEntity);
+//
+//        return "yourPage";
+//    }
+//}
