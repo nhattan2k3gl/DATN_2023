057ce6b8d3ce9e7e8166e5c97dcac8b5a8c10291
khoabaps <99518851+khoabaps@users.noreply.github.com>
2023-11-14 23:30:01 +0700

    commit DATN

--- src/main/java/com/example/demo/Dao/HoaDonChiTietDao.java
+++ src/main/java/com/example/demo/Dao/HoaDonChiTietDao.java
@@ -1,9 +1,13 @@
 package com.example.demo.Dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.Entity.HoaDonChiTietEntity; 
 
 public interface HoaDonChiTietDao extends JpaRepository<HoaDonChiTietEntity, String>  {
-
+	@Query(value = "select SUM(sanpham.gia*HoaDonChiTiet.SoLuong) as N'Tổng Tiền' from HoaDonChiTiet join SanPham on HoaDonChiTiet.ID_HD=sanpham.ID_SP", nativeQuery = true)
+	Integer thongke();
 }
